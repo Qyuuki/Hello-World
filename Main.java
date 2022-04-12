cefda7d2f4f8d7f7db7cc9ba24bccaefc5afc3d7
ooshima yuuki <toradosioosima@gmail.com>
2022-04-12 18:46:49 +0900

    first

---
+++ Main.java
@@ -0,0 +1,14 @@
+//Hello World
+public class Main {
+  public static void main(String[] args) {
+	  System.out.println("Hello World");
+	  
+	  for(int i = 0; i < 20; i++) {
+		  if(i < 10) {
+			  System.out.println("Hello World");
+		  }else {
+			  System.out.println("Hello");
+		  }
+	  }
+  }
+}
